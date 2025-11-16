d6782afd6bf7e8eefd7311cacdace009f5b2f91b
jmpop <pjm970128@gmail.com>
2025-11-16 16:30:18 +0900

    .

--- shareproject/settings.py
+++ shareproject/settings.py
@@ -22,7 +22,6 @@ dotenv.read_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
@@ -32,6 +31,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 DEBUG = True
 
 ALLOWED_HOSTS = json.loads(os.environ.get("ALLOWED_HOSTS", '["*"]'))
+SECURE_CROSS_ORIGIN_OPENER_POLICY = 'same-origin'
 
 # Application definition
 
