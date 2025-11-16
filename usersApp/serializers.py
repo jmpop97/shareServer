915a26b2e6abeb1467daaafef2af92a1802606c4
jmpop <pjm970128@gmail.com>
2025-11-16 16:17:40 +0900

    .

--- usersApp/serializers.py
+++ usersApp/serializers.py
@@ -2,6 +2,7 @@ from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
     @classmethod
     def get_token(cls, user):
+        print("token user | ",user)
         token = super().get_token(user)
         token['username'] = user.username
         return token
